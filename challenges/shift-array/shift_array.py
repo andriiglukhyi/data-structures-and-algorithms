a863c803ddb584702306caf9272940a77b6f5692
andriiglukhyi <andrigluxii@gmail.com>
2018-03-24 00:37:53 -0700

    new version of shift array

---
+++ challenges/shift-array/shift_array.py
@@ -0,0 +1,29 @@
+def insert_shift_array(array,value):
+    """algoritm should add element in array and shit tht array to the rigth"""
+    if type(value) != int or type(array) != list:
+        return False
+    elif len(array) == 0:
+        array.append(value)
+        return array
+    else:
+        if len(array) > 0 and len(array)%2 == 0: 
+            midle = len(array)//2
+        elif len(array)>0 and len(array)%2 == 1:
+            midle = len(array)//2 + 1
+        else:
+            midle = 0
+        new_array = [0]*(len(array)+1)
+        for item in range(midle):
+            new_array[item] = array[item]
+        new_array[midle] = value
+        for item in range(midle+1, len(new_array)):
+            new_array[item] = array[item-1]
+        array = new_array
+        print(array)
+    return array
+
+
+if __name__ == '__main__':
+    insert_shift_array(0, 1)
+
+     
---
+++ challenges/shift-array/shift_array_test.py
@@ -0,0 +1,34 @@
+import shift_array as s
+
+
+def test_shift_array_if_lenisodd():
+    """test the case when number of elements is odd number"""
+    array = [0,0,0]
+    value = 1
+    assert s.insert_shift_array(array, value) == [0,0,1,0]
+
+def test_shift_array_if_leniseven():
+    """test the case when number of elements is even number"""
+    array = [0,0,0,0]
+    value = 1
+    assert s.insert_shift_array(array, value)== [0,0,1,0,0]
+
+def test_shift_array_if_itisempty():
+    """test the case when array is empty"""
+    array = []
+    value = 1
+    assert s.insert_shift_array(array, value) == [1]
+
+def test_shift_array_value_not_int():
+    """test if value is not int"""
+    array = []
+    value =  'a'
+    assert s.insert_shift_array(array,value) == False
+
+def test_if_first_par_isnt_array():
+    arr = 1
+    val = 1
+    assert s.insert_shift_array(arr, val) == False
+
+
+
