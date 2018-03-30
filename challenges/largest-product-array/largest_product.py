191e19a749dd2bc528c9b8dcdccf389be95ba5b1
andriiglukhyi <andrigluxii@gmail.com>
2018-03-29 19:15:13 -0700

    fixed problem

--- challenges/largest-product-array/largest_product.py
+++ challenges/largest-product-array/largest_product.py
@@ -17,7 +17,6 @@ def largest_product(arr):
     return largest
 
 
-
 def node_inside(sub_arr, largest):
     """item in one node"""
     if len(sub_arr) == 1:
@@ -29,18 +28,11 @@ def node_inside(sub_arr, largest):
                 largest = sub_arr[item]*sub_arr[item+1]
         return largest
 
-def node_to_node(node1,node2,largest):
+
+def node_to_node(node1, node2, largest):
     """campare two items"""
     if len(node1) == len(node2):
         for a in range(len(node1)):
-            if node1[a]*node2[a]>largest:
-                largest = node1[a]*node2[a]>largest
+            if node1[a]*node2[a] > largest:
+                largest = node1[a]*node2[a]
     return largest
-
-
-
-
-
-
-if __name__ == '__main__':
-    largest_product([ [ 1, 2 ,3,4,5,5,6,6,7,7,8,8,8,8], [ 3, 4,4,8,3,6,7,2,4,5,6,7,8,9 ], [ 5, 6, 3,4,5,6,7,8,2,3,4,5,6,7 ], [ 7, 8, 4, 5,3,4,5,6,7,2,3,4,5,6 ] ])
