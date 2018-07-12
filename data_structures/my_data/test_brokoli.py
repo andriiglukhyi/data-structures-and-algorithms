16a230f7b5345c464d08b8c0ca861c2d32faf1b7
andriiglukhyi <andriigluxii@gmail.com>
2018-07-12 14:28:53 -0700

    brokoli data structure

--- data_structures/my_data/test_brokoli.py
+++ data_structures/my_data/test_brokoli.py
@@ -7,7 +7,7 @@ class Node():
         init function for node
         """
         self.val = val
-        self.buckets = [None] * val
+        self.buckets = {}
     def __repr__(self):
         """repr function for brokoli"""
         return str(self.val)
@@ -29,6 +29,21 @@ class Brokoli:
         """
         repr function for tree
         """
+        # ans = ""
+        # def add_string(node.val):
+        #     nonlocal ans
+        #     """
+        #     fucntion to add values to string
+        #     """
+        #     ans += str(node.val)
+        
+        # if self.root is None:
+        #     return ans
+        # else:
+        #     self.pre_order(self.root, add_string)
+        pass
+
+            
     
     def __str__(self):
         """
@@ -39,15 +54,19 @@ class Brokoli:
         """
         insert new element in brokoli
         """
+        def _insert(node, val):
+            """
+            helper function to insert value
+            """
+            
         if self.root is None:
             self.root = Node(val)
         else:
-            def _insert(self.root, val):
-                bucket = self.root.val % val
-                if self.root.buckets[bucket] is None:
-                    self.root.buckets[bucket] = Node(val)
-                else:
-                    _insert(self.root.buckets[bucket], val)
+            bucket = self.root.val % val
+            if bucket not in self.root.buckets:
+                self.root.buckets[bucket] = Node(val)
+            else:
+                self.insert(self.root.buckets[bucket], val)
     
     def pre_order(self, action=None):
         """
@@ -57,24 +76,46 @@ class Brokoli:
             if node is None:
                 return
             operation(node.val)
-            if node.buckets is not None:
-                _walk(node.left)
-            if node.right is not None:
-                _walk(node.right)
+            node_buckets = list(node.buckets.values())
+            if len(node.buckets) > 0:
+                for node in node_buckets:
+                    _walk(node)
+        if self.root is None:
+            return False
+        else:
+            _walk(self.root) 
     
-    def in_order(self, action=None):
+    def post_order(self, action=None):
         """
         inorder traversal
         """
-    
-    def post_order(self):
-        """
-        postorder traversal
-        """
-    def find(self, val):
-        """
-        check if exiting value in brokoli
-        """
+        def _walk(node=None):
+            if node is None:
+                return
+            node_buckets = list(node.buckets.values())
+            if len(node.buckets) > 0:
+                for node in node_buckets:
+                    _walk(node)
+            operation(node.val)
+        if self.root is None:
+            return False
+        else:
+            _walk(self.root)
+
+    # def find(self, val):
+    #     """
+    #     check if exiting value in brokoli
+    #     """
+    #     def _chek(node.val, val):
+    #         """
+    #         check if this is curent value
+    #         """
+    #         if node.val == val:
+    #             return True
+    #         return 
+    #     # use order traversal to traverse brokoli and find value 
+    #     self.pre_order(_check)
+        
 
 
     
