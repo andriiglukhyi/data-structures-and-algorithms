5ceb248ce8ee517fa1fb821e0f4949f5991ec8ac
andriiglukhyi <andriigluxii@gmail.com>
2018-07-24 13:21:14 -0700

    done with edges class

--- data_structures/graph/vertex.py
+++ data_structures/graph/vertex.py
@@ -1,19 +1,20 @@
 class Vertex:
-    __slots__ = 'element'
     """
     Ligthweight verttex structure for graph
     """
+    __slots__ = '_element'
+
     def __init__(self, x):
         """
         Do not call constractor dorectly. Use Graph's insert_vertex(x)
         """
-        self.element = x
-    
+        self._element = x
+        
     def element(self):
         """
         Return element associated with this vertex
         """
-        return self.element
+        return self._element
     
     def __hash__(self):
         """
