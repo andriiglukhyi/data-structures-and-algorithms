da787249384caa0330e7c08654a3723287ce4b4d
andriiglukhyi <andriigluxii@gmail.com>
2018-08-11 19:30:49 -0700

    belman-ford

---
+++ data_structures/graph/belma-ford.py
@@ -0,0 +1,31 @@
+def BellmanFord(self, src):
+     
+        # Step 1: Initialize distances from src to all other vertices
+        # as INFINITE
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+ 
+ 
+        # Step 2: Relax all edges |V| - 1 times. A simple shortest 
+        # path from src to any other vertex can have at-most |V| - 1 
+        # edges
+        for i in range(self.V - 1):
+            # Update dist value and parent index of the adjacent vertices of
+            # the picked vertex. Consider only those vertices which are still in
+            # queue
+            for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
+ 
+        # Step 3: check for negative-weight cycles.  The above step 
+        # guarantees shortest distances if graph doesn't contain 
+        # negative weight cycle.  If we get a shorter path, then there
+        # is a cycle.
+ 
+        for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        print "Graph contains negative weight cycle"
+                        return
+                         
+        # print all distance
+        self.printArr(dist)
